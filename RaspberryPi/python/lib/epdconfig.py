0e5dde3239ab7c5a08c47db5a2f70aa1a01dc2f4
Ryan Ziolko <rziolko@gmail.com>
2021-03-09 20:39:26 -0600

    Add initial support for using spidev on the Raspberry Pi platform. Use SPI.writebytes2() to send pixel data in builk.

--- RaspberryPi/python/lib/epdconfig.py
+++ RaspberryPi/python/lib/epdconfig.py
@@ -26,123 +26,121 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #
-import RPi.GPIO as GPIO
 import time
 import os
 import logging
 import sys
 
-from ctypes import *
 
-EPD_SCK_PIN   =11
-EPD_MOSI_PIN  =10
+class RaspberryPi:
 
-EPD_M1_CS_PIN  =8
-EPD_S1_CS_PIN  =7
-EPD_M2_CS_PIN  =17
-EPD_S2_CS_PIN  =18
+    # Pin definitions
+    EPD_M1_CS_PIN    = 8
+    EPD_S1_CS_PIN    = 7
+    EPD_M2_CS_PIN    = 17
+    EPD_S2_CS_PIN    = 18
 
-EPD_M1S1_DC_PIN  =13
-EPD_M2S2_DC_PIN  =22
+    EPD_M1S1_DC_PIN  = 13
+    EPD_M2S2_DC_PIN  = 22
 
-EPD_M1S1_RST_PIN =6
-EPD_M2S2_RST_PIN =23
+    EPD_M1S1_RST_PIN = 6
+    EPD_M2S2_RST_PIN = 23
 
-EPD_M1_BUSY_PIN  =5
-EPD_S1_BUSY_PIN  =19
-EPD_M2_BUSY_PIN  =27
-EPD_S2_BUSY_PIN  =24
+    EPD_M1_BUSY_PIN  = 5
+    EPD_S1_BUSY_PIN  = 19
+    EPD_M2_BUSY_PIN  = 27
+    EPD_S2_BUSY_PIN  = 24
 
-find_dirs = [
-    os.path.dirname(os.path.realpath(__file__)),
-    '/usr/local/lib',
-    '/usr/lib',
-]
-spi = None
-for find_dir in find_dirs:
-    so_filename = os.path.join(find_dir, 'DEV_Config.so')
-    if os.path.exists(so_filename):
-        spi = CDLL(so_filename)
-        break
-if spi is None:
-    RuntimeError('Cannot find DEV_Config.so')
+    def __init__(self):
+        import spidev
+        import RPi.GPIO
 
+        logging.debug("Using RaspberryPi implementation.")
 
-def digital_write(pin, value):
-    GPIO.output(pin, value)
+        self.GPIO = RPi.GPIO
+        self.SPI = spidev.SpiDev()
 
-def digital_read(pin):
-    return GPIO.input(pin)
+    def digital_write(self, pin, value):
+        self.GPIO.output(pin, value)
 
-def spi_writebyte(value): 
-    spi.DEV_SPI_WriteByte(value)
- 
-def delay_ms(delaytime):
-    time.sleep(delaytime / 1000.0)
-        
-def module_init():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(EPD_SCK_PIN, GPIO.OUT)    
-    GPIO.setup(EPD_MOSI_PIN, GPIO.OUT)
-    
-    logging.debug("python call wiringPi Lib")
-    
-    GPIO.setup(EPD_M2S2_RST_PIN, GPIO.OUT)    
-    GPIO.setup(EPD_M1S1_RST_PIN, GPIO.OUT)
-    GPIO.setup(EPD_M2S2_DC_PIN, GPIO.OUT)
-    GPIO.setup(EPD_M1S1_DC_PIN, GPIO.OUT)
-    GPIO.setup(EPD_S1_CS_PIN, GPIO.OUT)
-    GPIO.setup(EPD_S2_CS_PIN, GPIO.OUT)
-    GPIO.setup(EPD_M1_CS_PIN, GPIO.OUT)
-    GPIO.setup(EPD_M2_CS_PIN, GPIO.OUT)
+    def digital_read(self, pin):
+        return self.GPIO.input(pin)
 
-    GPIO.setup(EPD_S1_BUSY_PIN, GPIO.IN)
-    GPIO.setup(EPD_S2_BUSY_PIN, GPIO.IN)
-    GPIO.setup(EPD_M1_BUSY_PIN, GPIO.IN)
-    GPIO.setup(EPD_M2_BUSY_PIN, GPIO.IN)
-    
-    digital_write(EPD_M1_CS_PIN, 1)
-    digital_write(EPD_S1_CS_PIN, 1)
-    digital_write(EPD_M2_CS_PIN, 1)
-    digital_write(EPD_S2_CS_PIN, 1)
-    
-    digital_write(EPD_M2S2_RST_PIN, 0)
-    digital_write(EPD_M1S1_RST_PIN, 0)
-    digital_write(EPD_M2S2_DC_PIN, 1)
-    digital_write(EPD_M1S1_DC_PIN, 1)
+    def spi_writebyte(self, data):
+        self.SPI.writebytes(data)
 
-    spi.DEV_ModuleInit()
+    def spi_writebytes(self, data):
+        self.SPI.writebytes2(data)
 
-def module_exit():
-    digital_write(EPD_M2S2_RST_PIN, 0)
-    digital_write(EPD_M1S1_RST_PIN, 0)
-    digital_write(EPD_M2S2_DC_PIN, 0)
-    digital_write(EPD_M1S1_DC_PIN, 0)
-    digital_write(EPD_S1_CS_PIN, 1)
-    digital_write(EPD_S2_CS_PIN, 1)
-    digital_write(EPD_M1_CS_PIN, 1)
-    digital_write(EPD_M2_CS_PIN, 1)
+    def spi_readbyte(self):
+        bytes = self.SPI.readbytes(2)
+        logging.debug("spi_readbyte: %s" % (str(bytes)))
+        return bytes[0]
 
-def spi_readbyte(Reg):
-    GPIO.setup(EPD_MOSI_PIN, GPIO.IN)
-    j=0
-    # time.sleep(0.01)
-    for i in range(0, 8):
-        GPIO.output(EPD_SCK_PIN, GPIO.LOW) 
-        # time.sleep(0.01) 
-        j = j << 1 
-        if(GPIO.input(EPD_MOSI_PIN) == GPIO.HIGH):
-            j |= 0x01
-        else:
-            j &= 0xfe 
-        # time.sleep(0.01)
-        GPIO.output(EPD_SCK_PIN, GPIO.HIGH) 
-        # time.sleep(0.01)  
-    GPIO.setup(EPD_MOSI_PIN, GPIO.OUT)
-    return j 
-    
-    def delay_ms(delaytime):
+    def delay_ms(self, delaytime):
         time.sleep(delaytime / 1000.0)
 
+    def module_init(self):
+        self.GPIO.setmode(self.GPIO.BCM)
+        self.GPIO.setwarnings(False)
+
+        logging.debug("python call wiringPi Lib")
+
+        self.GPIO.setup(self.EPD_M2S2_RST_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.EPD_M1S1_RST_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.EPD_M2S2_DC_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.EPD_M1S1_DC_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.EPD_S1_CS_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.EPD_S2_CS_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.EPD_M1_CS_PIN, self.GPIO.OUT)
+        self.GPIO.setup(self.EPD_M2_CS_PIN, self.GPIO.OUT)
+
+        self.GPIO.setup(self.EPD_S1_BUSY_PIN, self.GPIO.IN)
+        self.GPIO.setup(self.EPD_S2_BUSY_PIN, self.GPIO.IN)
+        self.GPIO.setup(self.EPD_M1_BUSY_PIN, self.GPIO.IN)
+        self.GPIO.setup(self.EPD_M2_BUSY_PIN, self.GPIO.IN)
+
+        # SPI device, bus = 0, device = 0
+        self.SPI.open(0, 0)
+        self.SPI.max_speed_hz = 4000000
+        self.SPI.mode = 0b00
+
+        # Tell spidev we do not want the kernel SPI driver
+        # using the CE01 line
+        self.SPI.no_cs = True
+
+        self.digital_write(self.EPD_M1_CS_PIN, 1)
+        self.digital_write(self.EPD_S1_CS_PIN, 1)
+        self.digital_write(self.EPD_M2_CS_PIN, 1)
+        self.digital_write(self.EPD_S2_CS_PIN, 1)
+
+        self.digital_write(self.EPD_M2S2_RST_PIN, 0)
+        self.digital_write(self.EPD_M1S1_RST_PIN, 0)
+        self.digital_write(self.EPD_M2S2_DC_PIN, 1)
+        self.digital_write(self.EPD_M1S1_DC_PIN, 1)
+
+        return 0
+
+    def module_exit(self):
+        self.SPI.close()
+        logging.debug("close 5V, Module enters 0 power consumption ...")
+
+        self.GPIO.output(self.EPD_M1S1_RST_PIN, 0)
+        self.GPIO.output(self.EPD_M2S2_RST_PIN, 0)
+        self.GPIO.output(self.EPD_M2S2_DC_PIN, 0)
+        self.GPIO.output(self.EPD_M1S1_DC_PIN, 0)
+
+        self.digital_write(self.EPD_S1_CS_PIN, 1)
+        self.digital_write(self.EPD_S2_CS_PIN, 1)
+        self.digital_write(self.EPD_M1_CS_PIN, 1)
+        self.digital_write(self.EPD_M2_CS_PIN, 1)
+
+        self.GPIO.cleanup()
+
   
+
+if os.path.exists('/sys/bus/platform/drivers/gpiomem-bcm2835'):
+    implementation = RaspberryPi()
+
+for func in [x for x in dir(implementation) if not x.startswith('_')]:
+    setattr(sys.modules[__name__], func, getattr(implementation, func))
