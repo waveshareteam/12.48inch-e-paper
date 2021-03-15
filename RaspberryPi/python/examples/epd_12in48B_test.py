98d52edb849ecb9eb1e168178d6de62e498cca2b
Ryan Ziolko <rziolko@gmail.com>
2021-03-15 15:02:43 -0500

    Update the epd12in48b demo application to draw using Black, turn on DEBUG logger, and handle any exceptions when drawing to the panel.

--- RaspberryPi/python/examples/epd_12in48B_test.py
+++ RaspberryPi/python/examples/epd_12in48B_test.py
@@ -1,67 +1,67 @@
 ﻿#!/usr/bin/python
 # -*- coding:utf-8 -*-
 
+import logging
+logging.basicConfig(level = logging.DEBUG)
+
+import time
 import sys
 import os
 picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
 libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
 if os.path.exists(libdir):
     sys.path.append(libdir)
-
 import epd12in48b
-import time
-
-from PIL import ImageDraw
-from PIL import ImageFont
-from PIL import ImageColor
 
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 print("12.48inch e-paper B Demo...")
 epd = epd12in48b.EPD()
-# Initialize library.
-epd.Init()
+try:
+    epd.Init()
+    print("clearing...")
+    epd.clear()
 
-# Clear epdlay.
-print("clearing...")
-epd.clear()
+    # Create blank image for drawing.
+    Blackimage = Image.new("1", (epd12in48b.EPD_WIDTH, epd12in48b.EPD_HEIGHT), 255)
+    Redimage = Image.new("1", (epd12in48b.EPD_WIDTH, epd12in48b.EPD_HEIGHT), 255)
+    Blackdraw = ImageDraw.Draw(Blackimage)
+    Reddraw = ImageDraw.Draw(Redimage)
 
-# Create blank image for drawing.
-Blackimage = Image.new("1", (epd12in48b.EPD_WIDTH, epd12in48b.EPD_HEIGHT), 255)
-Redimage = Image.new("1", (epd12in48b.EPD_WIDTH, epd12in48b.EPD_HEIGHT), 255)
-Blackdraw = ImageDraw.Draw(Blackimage)
-Reddraw = ImageDraw.Draw(Redimage)
+    font = ImageFont.truetype(picdir+"/Font.ttc", 300)
+    font10 = ImageFont.truetype(picdir+"/Font.ttc",60)
 
-font = ImageFont.truetype(picdir+"/Font.ttc", 300)
-font10 = ImageFont.truetype(picdir+"/Font.ttc",60)
+    print("drawing...")
+    Blackdraw.rectangle([(40,40),(440,440)],fill = "Black",)
+    Blackdraw.rectangle([(43,43),(437,437)],fill = "White")
+    Blackdraw.line([(40,40),(440,440)], fill = "Black",width = 3)
+    Blackdraw.line([(440,40),(40,440)], fill = "Black",width = 3)
+    Reddraw.arc([50, 50, 430, 430], 0, 360, fill = "Black")
+    Reddraw.text((60,500), u'微雪电子 ', font = font, fill = "Black")
+    Reddraw.text((600, 90), 'Waveshare Electronic ', font = font10, fill = "Black")
+    Reddraw.text((600, 170), '12.48 inch e-paper ', font = font10, fill = "Black")
+    Reddraw.text((600, 250), 'Test Program ', font = font10, fill = "Black")
 
-print("drawing...")
-Blackdraw.rectangle([(40,40),(440,440)],fill = "BLUE",)
-Blackdraw.rectangle([(43,43),(437,437)],fill = "WHITE")
-Blackdraw.line([(40,40),(440,440)], fill = "BLUE",width = 3)
-Blackdraw.line([(440,40),(40,440)], fill = "BLUE",width = 3)
-Reddraw.arc([50, 50, 430, 430], 0, 360, fill = "BLUE")
-Reddraw.text((60,500), u'微雪电子 ', font = font, fill = "BLUE")
-Reddraw.text((600, 90), 'Waveshare Electronic ', font = font10, fill = "BLUE")
-Reddraw.text((600, 170), '12.48 inch e-paper ', font = font10, fill = "BLUE")
-Reddraw.text((600, 250), 'Test Program ', font = font10, fill = "BLUE")
+    Blackimage=Blackimage.rotate(180)
+    Redimage=Redimage.rotate(180)
+    epd.display(Blackimage, Redimage)
+    time.sleep(2)
 
-Blackimage=Blackimage.rotate(180) 
-Redimage=Redimage.rotate(180) 
-epd.display(Blackimage, Redimage)
-time.sleep(2)
+    print("open pic...")
+    Blackimage2 = Image.new("1", (epd12in48b.EPD_WIDTH, epd12in48b.EPD_HEIGHT), 255)
+    Redimage2 = Image.new("1", (epd12in48b.EPD_WIDTH, epd12in48b.EPD_HEIGHT), 255)
+    Redimage2 = Image.open(picdir+"/1304x984.jpg")	
+    epd.display(Blackimage2, Redimage2)
+    time.sleep(2)
 
-print("open pic...")
-Blackimage2 = Image.new("1", (epd12in48b.EPD_WIDTH, epd12in48b.EPD_HEIGHT), 255)
-Redimage2 = Image.new("1", (epd12in48b.EPD_WIDTH, epd12in48b.EPD_HEIGHT), 255)
-Redimage2 = Image.open(picdir+"/1304x984.jpg")	
-epd.display(Blackimage2, Redimage2)
-time.sleep(2)
+    # Clear the sceen to white for long term storage
+    print("clearing...")
+    epd.clear()
 
-# Do not use for a long time, please save the white screen to save
-print("clearing and go to sleep...")
-epd.clear()
-epd.EPD_Sleep()
-
-
-
+    print("goto sleep...")
+    epd.EPD_Sleep()
+except:
+    print("goto sleep...")
+    epd.EPD_Sleep()
